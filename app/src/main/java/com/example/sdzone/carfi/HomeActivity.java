6156e82d74f2bc18a33345f29f41f72d12b2877a
sdzone <30996229+sdzone@users.noreply.github.com>
2018-07-06 02:03:57 +0530

    sd

--- app/src/main/java/com/example/sdzone/carfi/HomeActivity.java
+++ app/src/main/java/com/example/sdzone/carfi/HomeActivity.java
@@ -8,12 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 
 public class HomeActivity extends AppCompatActivity {
     private EditText Name;
@@ -56,25 +51,25 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    // Instantiate the RequestQueue.
-    RequestQueue queue = Volley.newRequestQueue(this);
-    String url ="http://www.google.com";
-
-    // Request a string response from the provided URL.
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                   // mTextView.setText("Response is: "+ response.substring(0,500));
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            //mTextView.setText("That didn't work!");
-        }
-    });
-
-// Add the request to the RequestQueue.
-queue.add(stringRequest);
+//    // Instantiate the RequestQueue.
+//    RequestQueue queue = Volley.newRequestQueue(this);
+//    String url ="http://www.google.com";
+//
+//    // Request a string response from the provided URL.
+//    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//            new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    // Display the first 500 characters of the response string.
+//                   // mTextView.setText("Response is: "+ response.substring(0,500));
+//                }
+//            }, new Response.ErrorListener() {
+//        @Override
+//        public void onErrorResponse(VolleyError error) {
+//            //mTextView.setText("That didn't work!");
+//        }
+//    });
+//
+//// Add the request to the RequestQueue.
+////queue.add(stringRequest);
 }
